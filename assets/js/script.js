37ec3e43c2b63454834c9f610b8f10dd7c3210ef
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2024-01-18 18:53:58 -0600

    Added flavor, projects, and navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+
+// Disable scrolling
+document.body.style.overflow = 'hidden';
+
 setTimeout(async () => {
   // await move();
   // Check if the document's readyState is complete after the move function is done animating
@@ -9,12 +13,52 @@ setTimeout(async () => {
         "transition": "opacity 1s ease-out",
         "opacity": 0
       });
+
       setTimeout(() => {
         loader.css("display", 'none');
-      }, 1000); // This should match the transition duration
-      const mainPage = $('#main_page');
-      mainPage.css("opacity", 1);
+
+        const mainPage = $('#main_page');
+        mainPage.css("display", 'block');
+
+        setTimeout(() => {
+          mainPage.css("opacity", 1);
+        }, 500);  // This should match the transition duration for opacity
+
+      }, 1000);  // This should match the transition duration for opacity
+
+
+
       document.body.style.overflow = 'auto'; // Enable scrolling again
+
+
     }, 5000);
+
+
   }
 }, 1000); // Replace 1000 with the number of milliseconds you want to delay
+
+
+window.addEventListener('scroll', function() {
+  var arrow = document.getElementById('arrow_ctn');
+  if (window.scrollY > 0) {
+    // User has scrolled down, hide the arrow
+    arrow.style.opacity = '0';
+    navbar.classList.add('navbar-visible');
+  } else {
+    // User has scrolled to the top, show the arrow
+    arrow.style.opacity = '1';
+    navbar.classList.remove('navbar-visible');
+  }
+});
+
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   const navbar = document.getElementById('navbar');
+//   window.addEventListener('scroll', () => {
+//     if (window.scrollY > 0) {
+//       navbar.classList.add('navbar-visible');
+//     } else {
+//       navbar.classList.remove('navbar-visible');
+//     }
+//   });
+// });
